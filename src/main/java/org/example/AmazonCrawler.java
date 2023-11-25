9409fc2c0371bdd1e8a7ed4860f672aea1cf8d18
Pavel-OOP <pavel.i.petrov.7@gmail.com>
2023-11-25 17:27:43 +0200

    wrote example write file function

--- src/main/java/org/example/AmazonCrawler.java
+++ src/main/java/org/example/AmazonCrawler.java
@@ -3,15 +3,19 @@ package org.example;
 import org.jsoup.nodes.Document;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class AmazonCrawler {
-    private static final SimpleDateFormat timeStampNew = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss");
-     public static void main(String[] args) {
+    private static final SimpleDateFormat TIME_STAMP_NEW = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss");
 
+     public static void main(String[] args) {
+        AmazonCrawler.writeLog("example info");
     }
     private static void amazonCrawl(int depth, String url, ArrayList<String> visitedUrls){
 
@@ -27,27 +31,29 @@ public class AmazonCrawler {
     }
 
     public static void writeLog(String info) {
+        Date date = new Date();
+        Timestamp timeStamp = new Timestamp(date.getTime());
+        String timestamp = TIME_STAMP_NEW.format(timeStamp);
 
-        String filename = "activity.log";
-        String FILENAME = "C:\\testing\\" + filename;
-        BufferedWriter bw = null;
-        FileWriter fw = null;
+        String filename = timestamp + "_results.txt";
+
+        BufferedWriter output = null;
         try {
-            fw = new FileWriter(FILENAME, true);
-            bw = new BufferedWriter(fw);
-            bw.write(info);
-            bw.write("\n");
-        } catch (IOException e) {
+            new File("tmp").mkdirs();
+            File tempfile = new File(System.getProperty("user.dir") + File.separator + "tmp", filename);
+            output = new BufferedWriter(new FileWriter(tempfile));
+            output.write(info);
+        } catch ( IOException e ) {
             e.printStackTrace();
         } finally {
-            try {
-                if (bw != null)
-                    bw.close();
-                if (fw != null)
-                    fw.close();
-            } catch (IOException ex) {
-                ex.printStackTrace();
+            if ( output != null ) {
+                try {
+                    output.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
             }
         }
+
     }
 }
