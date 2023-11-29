3d0de5e1f9f4618d8b1af2c61270d4a631ef5fa8
Pavel-OOP <pavel.i.petrov.7@gmail.com>
2023-11-29 18:46:54 +0200

    getting response status and body info

--- src/test/java/TestPostsAPI.java
+++ src/test/java/TestPostsAPI.java
@@ -1,21 +1,40 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.net.URI;
 import java.net.URL;
+import java.util.Scanner;
 
 public class TestPostsAPI {
     private URL url;
     private HttpURLConnection con;
+
     @Before
     public void setUp(){
         try{
-            url = new URL("https://jsonplaceholder.typicode.com/posts");
+            url = new URI("https://jsonplaceholder.typicode.com/posts").toURL();
             con = (HttpURLConnection) url.openConnection();
             con.setRequestMethod("GET");
-        }catch (Exception ignore){}
+            con.connect();
+
+            StringBuilder respInfo = new StringBuilder();
+            Scanner scanner = new Scanner(url.openStream());
+
+            while(scanner.hasNext()){
+                respInfo.append(scanner.nextLine());
+            }
+            scanner.close();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     @Test
-    public void testPostsAPI(){}
+    public void testPostsAPI() throws IOException {
+        int status = con.getResponseCode();
+        System.out.println(status);
+    }
 }
